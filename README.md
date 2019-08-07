c7dd3c94a312d3ecb0b0c5817e0fdac98b8e54e6
Jordon Dyer <jordon@dighroieg9843.local>
2019-08-07 15:06:07 +0100

    7/8/19

--- README.md
+++ README.md
@@ -2,18 +2,18 @@
 
   * Scrape images from any public instagram account via terminal and save to a given directory
   * Version 1.0
-  
-  
-# Todo..
+
+
+# Todo
 
   * Get ig account analytics. (followers, likes, following etc..)
   * Top posts, most likes.
 
 
-## Usage..
+## Usage
   * In terminal, run 'pip install -r requirements.txt' in directory of requirements.txt
-  * Then run python3 main.py and follow user input to downloade images.
-  
-  
-  
+  * Then run python3 main.py and follow user input to download images.
+
+
+
 @author Jordon Dyer
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os,re,time
+import os
+import re
+import time
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 import urllib
@@ -6,40 +8,47 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
 
+
 def get_image_urls(username):
-    links=[]
+    links = []
     options = Options()
     options.add_argument("--headless")
-    browser = webdriver.Chrome('/Users/jordon/Downloads/chromedriver',options=options)
-    browser.get('https://www.instagram.com/'+username+'/?hl=en')
-    Pagelength = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+    browser = webdriver.Chrome(
+        '/Users/jordon/Downloads/chromedriver', options=options)
+    browser.get('https://www.instagram.com/' + username + '/?hl=en')
+    Pagelength = browser.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);")
+    lenOfPage = browser.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
     print('Please wait, scrolling now.')
     # Infinte Scroll until reaches very bottom
-    match=False
-    while(match==False):
+    match = False
+    while(match == False):
         lastCount = lenOfPage
 
         # Get image links while scrolling (doesn't work otherwise)
         source = browser.page_source
-        data=bs(source, 'html.parser')
+        data = bs(source, 'html.parser')
         body = data.find('body')
         script = body.find('span')
         for link in script.findAll('a'):
             if re.match("/p", link.get('href')):
-                links.append('https://www.instagram.com'+link.get('href'))
+                links.append('https://www.instagram.com' + link.get('href'))
         time.sleep(3)
-        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        if lastCount==lenOfPage:
+        lenOfPage = browser.execute_script(
+            "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+        if lastCount == lenOfPage:
             source = browser.page_source
-            data=bs(source, 'html.parser')
+            data = bs(source, 'html.parser')
             body = data.find('body')
             script = body.find('span')
             for link in script.findAll('a'):
                 if re.match("/p", link.get('href')):
-                    links.append('https://www.instagram.com'+link.get('href'))
-            match=True
+                    links.append('https://www.instagram.com' +
+                                 link.get('href'))
+            match = True
     return links
+
 
 def download_images():
     username = input('Enter instagram username to scrape: ')
@@ -50,13 +59,14 @@ def download_images():
     os.mkdir(folder_name)
     for x in images:
         file_name = x.split('/')[-2]
-        fullfilename = os.path.join(folder_name, file_name+'.jpg')
+        fullfilename = os.path.join(folder_name, file_name + '.jpg')
         test = requests.get(x)
         html = test.text
-        data = bs(html,'html.parser')
-        image = data.find('meta',property='og:image')
+        data = bs(html, 'html.parser')
+        image = data.find('meta', property='og:image')
         image_url = image['content']
-        urllib.request.urlretrieve(image_url,fullfilename)
+        urllib.request.urlretrieve(image_url, fullfilename)
+
 
 def main():
     download_images()
